e8d776c6615f4ceaa4de54e11fc78f414d0689d0
alexricardo02 <brinckmannalex@gmail.com>
2026-06-04 00:17:17 +0200

    feat: Global Exception Handler

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -31,44 +31,19 @@ public class UserController {
 	
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO loginDTO) {
-	    try {
-	        // Llamamos al servicio para validar credenciales
-	        UserProfileDTO userProfile = userService.login(loginDTO);
-	        
-	     // 2. Si es correcto, generamos el Token usando el username
-            String token = jwtUtil.generateToken(userProfile.getUsername());
-            
-         // 3. Empaquetamos todo en nuestro nuevo DTO
-            LoginResponseDTO response = new LoginResponseDTO(token, userProfile);
-	        
-	        // Por ahora devolvemos el perfil. 
-	        // El siguiente paso será devolver el TOKEN (JWT) aquí.
-	        return ResponseEntity.ok(response);
-	        
-	    } catch (SecurityException e) {
-	        // Si las credenciales fallan, devolvemos un 401 (No autorizado)
-	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-	    } catch (Exception e) {
-	        // 4. Captura cualquier otro error inesperado (500)
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	                             .body("Ocurrió un error inesperado");
-	    }
+		UserProfileDTO userProfile = userService.login(loginDTO);
+	    String token = jwtUtil.generateToken(userProfile.getUsername());
+	    LoginResponseDTO response = new LoginResponseDTO(token, userProfile);
+	    
+	    return ResponseEntity.ok(response);
 	}
 	
 	@PostMapping("/register")
 	public ResponseEntity<?> register(@Valid @RequestBody UserRegistrationDTO registrationDTO) {
-	    try {
-	        // Llamamos al método que ya tenías en el Service
-	        UserProfileDTO newUser = userService.saveUser(registrationDTO);
-	        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
-	    } catch (IllegalArgumentException e) {
-	        // Captura errores como "Email ya registrado" o "Usuario tomado"
-	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-	    } catch (Exception e) {
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al procesar el registro");
-	    }
+		UserProfileDTO newUser = userService.saveUser(registrationDTO);
+	    
+	    return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
 	}
 	
 	
-
 }
