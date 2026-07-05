c2f9b0bd22f8f8ff157945bb4c7129f2e64f4870
alexricardo02 <brinckmannalex@gmail.com>
2026-07-05 13:09:59 +0200

    unify names

--- src/main/java/com/example/controllers/ExpenseController.java
+++ src/main/java/com/example/controllers/ExpenseController.java
@@ -1,10 +1,5 @@
 package com.example.controllers;
 
-import java.util.List; 
-
-
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
